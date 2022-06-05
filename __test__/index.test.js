f21fa933898328ecb9a295473029f6a26593eac4
Arthur-codeX <daniel.karanja239@gmail.com>
2022-06-06 01:48:26 +0300

    Created Tajiri Mpesa Password Generator

---
+++ __test__/index.test.js
@@ -0,0 +1,27 @@
+const pass = require("./../index");
+
+describe("Mpesa Passwrod Generator", () => {
+  test("It should return an object containing password and time", () => {
+    const payload = {
+      BUSINESS_SHORT_CODE: 34546456,
+      PASS_KEY: "dsadnaskljnjsdnjfnsdjgnfkgdngdf",
+    };
+
+    const result = pass(payload);
+
+    expect(result).toHaveProperty("time");
+    expect(result).toHaveProperty("password");
+  });
+
+  test("It should return the given time when time is passed", () => {
+    const payload = {
+      BUSINESS_SHORT_CODE: 34546456,
+      PASS_KEY: "dsadnaskljnjsdnjfnsdjgnfkgdngdf",
+    };
+
+    const date = Date.now();
+    const result = pass(payload, date);
+
+    expect(result).toHaveProperty("time", date.toString());
+  });
+});
---
+++ index.js
@@ -0,0 +1,11 @@
+const time = require("tajiri-mpesa-time");
+const encode = require("nodejs-base64-encode");
+
+const Password = (conf, t = null, gmt = 3) => {
+  t === null ? (t = time(gmt)) : (t = t);
+  let data = `${(conf.BUSINESS_SHORT_CODE, conf.PASS_KEY)}`;
+  let en = encode.encode(data, "base64");
+  return { time: t.toString(), password: en };
+};
+
+module.exports = Password;
