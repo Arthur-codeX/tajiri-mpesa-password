e01f2e30929f08ad54f943c0788e4babcb6b8cc0
Arthur-codeX <daniel.karanja239@gmail.com>
2022-06-07 22:50:06 +0300

    Changed the format of retrieveing the time stamp

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const encode = require("nodejs-base64-encode");
 
 const Password = (conf, t = null, gmt = 3) => {
   t === null ? (t = time(gmt)) : (t = t);
-  let data = `${(conf.BUSINESS_SHORT_CODE, conf.PASS_KEY)}`;
+  // let data = `${(conf.BUSINESS_SHORT_CODE, conf.PASS_KEY)}`;
+  let data = `${conf.BUSINESS_SHORT_CODE}${conf.PASS_KEY}${t}`;
   let en = encode.encode(data, "base64");
   return { time: t.toString(), password: en };
 };
